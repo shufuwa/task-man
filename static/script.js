bdcfb73e24928ce2070f4f0127e2fb9b117e1f5d
shufuwa <shugobatista@protonmail.com>
2023-03-14 12:20:02 +0530

    js file

---
+++ static/script.js
@@ -0,0 +1,9 @@
+function showTasks() {
+    document.getElementById("tasks-view").style.display = "block";
+    document.getElementById("tasks-done-view").style.display = "none";
+}
+
+function showTasksDone() {
+    document.getElementById("tasks-view").style.display = "none";
+    document.getElementById("tasks-done-view").style.display = "block";
+}
