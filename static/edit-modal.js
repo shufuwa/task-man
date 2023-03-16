06ccbca9c92ef037b867d064f05f50d29ce767a2
shufuwa <shugobatista@protonmail.com>
2023-03-16 09:20:04 +0530

    edit working

--- static/edit-modal.js
+++ static/edit-modal.js
@@ -7,49 +7,83 @@ var editModal = document.getElementById("edit-modal");
 var closeEditModal = document.getElementById("close-modal-btn-2");
 
 // Select all edit links
-document.addEventListener('DOMContentLoaded', function () {
-    let taskTab = document.getElementById("tasks-view");
-    // console.log(taskTab);
-    if (taskTab) {
-        // console.log("tasks-view exists");
-        taskTab.addEventListener("click", function (event) {
-            console.log(event);
-            if (event.target.classList.contains("feather-8-edit")) {
-                console.log("Edit link clicked:", event.target.getAttribute("data-task-id"));
-                var taskId = event.target.getAttribute("data-task-id");
+// document.addEventListener('DOMContentLoaded', function () {
+//     let taskTab = document.getElementById("tasks-view");
+//     // console.log(taskTab);
+//     if (taskTab) {
+//         // console.log("tasks-view exists");
+//         taskTab.addEventListener("click", function (event) {
+//             console.log(event);
+//             if (event.target.classList.contains("feather-8-edit")) {
+//                 console.log("Edit link clicked:", event.target.getAttribute("data-task-id"));
+//                 var taskId = event.target.getAttribute("data-task-id");
 
-                // Make a Fetch request to get the task data from the database
-                fetch("/tasks/" + taskId, { method: "POST" })
-                    .then(function (response) {
-                        if (response.ok) {
-                            return response.json();
-                        } else {
-                            throw new Error("Network response was not ok.");
-                        }
-                    })
-                    .then(function (task) {
-                        // Fill in the form fields with the task data
-                        document.getElementById("edit_task_id").value = task.id;
-                        document.getElementById("edit_title").value = task.title;
-                        document.getElementById("edit_task_order").value = task.task_order;
-                        document.getElementById("edit_project").value = task.project;
-                        document.getElementById("edit_project_order").value = task.project_order;
+//                 // Make a Fetch request to get the task data from the database
+//                 fetch("/tasks/" + taskId, { method: "POST" })
+//                     .then(function (response) {
+//                         if (response.ok) {
+//                             return response.json();
+//                         } else {
+//                             throw new Error("Network response was not ok.");
+//                         }
+//                     })
+//                     .then(function (task) {
+//                         // Fill in the form fields with the task data
+//                         document.getElementById("edit_task_id").value = task.id;
+//                         document.getElementById("edit_title").value = task.title;
+//                         document.getElementById("edit_task_order").value = task.task_order;
+//                         document.getElementById("edit_project").value = task.project;
+//                         document.getElementById("edit_project_order").value = task.project_order;
 
-                        // log all the above variables
-                        console.log("edit_task_id: ", document.getElementById("edit_task_id").value);
-                        console.log("edit_title: ", document.getElementById("edit_title").value);
-                        console.log("edit_task_order: ", document.getElementById("edit_task_order").value);
-                        console.log("edit_project: ", document.getElementById("edit_project").value);
-                        console.log("edit_project_order: ", document.getElementById("edit_project_order").value);
-                    })
-                    .catch(function (error) {
-                        console.error("Error fetching task data:", error);
-                    });
+//                         // log all the above variables
+//                         console.log("edit_task_id: ", document.getElementById("edit_task_id").value);
+//                         console.log("edit_title: ", document.getElementById("edit_title").value);
+//                         console.log("edit_task_order: ", document.getElementById("edit_task_order").value);
+//                         console.log("edit_project: ", document.getElementById("edit_project").value);
+//                         console.log("edit_project_order: ", document.getElementById("edit_project_order").value);
+//                     })
+//                     .catch(function (error) {
+//                         console.error("Error fetching task data:", error);
+//                     });
+//             }
+//             else {
+//                 console.log("feather-8-edit not found");
+//             }
+
+//         });
+//     }
+// });
+
+function fetchTaskData(taskId) {
+    // Make a Fetch request to get the task data from the database
+    fetch("/tasks/" + taskId, { method: "POST" })
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error("Network response was not ok.");
             }
+        })
+        .then(function (task) {
+            // Fill in the form fields with the task data
+            document.getElementById("edit_task_id").value = task.id;
+            document.getElementById("edit_title").value = task.title;
+            document.getElementById("edit_task_order").value = task.task_order;
+            document.getElementById("edit_project").value = task.project;
+            document.getElementById("edit_project_order").value = task.project_order;
 
+            // log all the above variables
+            console.log("edit_task_id: ", document.getElementById("edit_task_id").value);
+            console.log("edit_title: ", document.getElementById("edit_title").value);
+            console.log("edit_task_order: ", document.getElementById("edit_task_order").value);
+            console.log("edit_project: ", document.getElementById("edit_project").value);
+            console.log("edit_project_order: ", document.getElementById("edit_project_order").value);
+        })
+        .catch(function (error) {
+            console.error("Error fetching task data:", error);
         });
-    }
-});
+}
+
 
 // When the user clicks the close button, close the edit modal
 closeEditModal.onclick = function () {
@@ -67,17 +101,23 @@ window.onclick = function (event) {
 var editForm = document.getElementById("edit_task");
 editForm.addEventListener("submit", function (event) {
     event.preventDefault();
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            // Reload the page to show the updated task list
-            location.reload();
-        }
-    };
     var formData = new FormData(editForm);
     var task_id = document.getElementById("edit_task_id").value;
-    console.log(formData);
-    xhr.open("POST", "/edit_task/" + task_id, true);
-    xhr.send(formData);
+    fetch("/edit_task/" + task_id, {
+        method: "POST",
+        body: formData
+    })
+    .then(function (response) {
+        if (response.ok) {
+            // Reload the page to show the updated task list
+            location.reload();
+        } else {
+            throw new Error("Network response was not ok.");
+        }
+    })
+    .catch(function (error) {
+        console.error("Error updating task:", error);
+    });
 });
+
 
